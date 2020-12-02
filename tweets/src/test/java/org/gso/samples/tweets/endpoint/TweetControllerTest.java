d0d5b714bb021b0ade2d02c2add8ea9acd3c28f7
Guillaume Soing <guillaume.soing@renault.com>
2020-12-02 07:43:01 +0100

    Add unitary test

--- tweets/src/test/java/org/gso/samples/tweets/endpoint/TweetControllerTest.java
+++ tweets/src/test/java/org/gso/samples/tweets/endpoint/TweetControllerTest.java
@@ -1,21 +1,28 @@
 package org.gso.samples.tweets.endpoint;
 
+import java.util.List;
+
+import org.gso.samples.tweets.model.Tweet;
+import org.gso.samples.tweets.model.User;
 import org.gso.samples.tweets.service.TweetService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.*;
 
-@WebMvcTest
-@AutoConfigureMockMvc
+@WebMvcTest(TweetsController.class)
 public class TweetControllerTest {
-
-    @MockBean
-    private TweetService tweetService;
 
     @Autowired
     private TweetsController tweetsController;
@@ -23,19 +30,21 @@ public class TweetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private TweetService tweetService;
+
     @Test
     public void whenTweetControlletInjected_thenNotNull() throws Exception {
         assertThat(tweetsController).isNotNull();
     }
 
-//    @Test
-//    public void whenNoTweet_thenResponseEmpty() throws Exception {
-//        Page<TweetDto> page = Page.<TweetDto>empty();
-//        page.getContent().add(TweetDto.builder().text("toto").build());
-//        given(tweetService.getTweets(null, Pageable.unpaged())).willReturn(page);
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/tweets")
-//                .contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8));
-//    }
+    @Test
+    public void whenNoTweet_thenResponseEmpty() throws Exception {
+        Page<Tweet> page = Page.empty();
+        given(tweetService.getTweets(new Criteria(), PageRequest.of(0, 20))).willReturn(page);
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/tweets")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data", Matchers.hasSize(0)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
